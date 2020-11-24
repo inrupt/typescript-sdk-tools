cdfb82cb5de3ef895250daab59668436ebc43f35
Matthieu Bosquet <matthieubosquet@gmail.com>
2020-11-24 19:51:04 +0000

    Leave default export for discussion

--- eslint-config-base/index.js
+++ eslint-config-base/index.js
@@ -84,9 +84,9 @@ module.exports = {
       "devDependencies": ["**/*.test.*"],
     }],
 
-    // Prefer no default exports
-    "import/prefer-default-export": 0,
-    "import/no-default-export": 2,
+    // TODO: Discuss whether we should we prefer default exports (as in airbnb base) or no default exports (as follows)
+    // "import/prefer-default-export": 0,
+    // "import/no-default-export": 2,
 
     // Remove airbnb's ForOfStatement recommendation; we don't use regenerator-runtime anywyas,
     // and we iterate over Sets in our libraries.
