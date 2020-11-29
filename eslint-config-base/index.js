067a15437b4a9e05186599eb9709f424258d1b5b
Matthieu Bosquet <matthieubosquet@gmail.com>
2020-11-29 22:54:26 +0000

    A few more rules

--- eslint-config-base/index.js
+++ eslint-config-base/index.js
@@ -23,18 +23,29 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
 
-  // Airbnb base provides many style rules; it is then overridden by our current defaults
-  // (jest, eslint, typescript, and finally prettier recommended configs.)
+  /**
+   * Extends (overrides in order of extension):
+   * - ESLint recommended https://eslint.org/docs/rules/
+   * - Airbnb base https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base
+   * - TypeScript recommended+type checking https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/src/configs
+   * - ES6+ export/imports https://github.com/benmosher/eslint-plugin-import/tree/master/config
+   * - Jest
+   * - Prettier
+   */
   extends: [
-    "airbnb-base",
     "eslint:recommended",
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
     "plugin:import/typescript",
     "plugin:jest/recommended",
     "plugin:jest/style",
-    "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier/@typescript-eslint",
   ],
 
@@ -51,6 +62,9 @@ module.exports = {
     SharedArrayBuffer: "readonly",
   },
 
+  // Ignore files and directories
+  ignorePatterns: ["**/dist/**/*", "**/coverage/**/*"],
+
   parser: "@typescript-eslint/parser",
 
   // Load typescript rules to handle es6 and typescript
@@ -59,6 +73,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
+
 
   settings: {
     "import/resolver": {
@@ -69,57 +84,178 @@ module.exports = {
     },
   },
 
+  /**
+   * Rules configuration
+   * - ESLint https://eslint.org/docs/rules/
+   * - ES6+ export/imports https://github.com/benmosher/eslint-plugin-import
+   * - TypeScript https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
+   * 
+   * Note: Use rule severity "off", "warn" & "error" instead of their numeric equivalent 0, 1 & 2
+   */
   rules: {
-    // Don't allow overwriting built-in globals like URL
-    "no-shadow": ["error", { "builtinGlobals": true }],
+    // ESLint
+    /**
+     * Syntax restrictions
+     * 
+     * Note: Removes airbnb's ForOfStatement recommendation; we don't use regenerator-runtime anyway,
+     * and we iterate over Sets in our libraries. Avoids TypeScript features that generate runtime
+     * code, it aligns with the TypeScript design goals stated in
+     * https://github.com/Microsoft/TypeScript/wiki/TypeScript-Design-Goals#goals.
+     * So no enum, namespace (taken care of by @typescript-eslint/recommended), parameter properties,
+     * or experimental decorators (TODO).
+     *  
+     * See also: https://eslint.org/docs/rules/no-restricted-syntax#disallow-specified-syntax-no-restricted-syntax
+     */
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "ForInStatement",
+        message: "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+      },
+      {
+        selector: "LabeledStatement",
+        message: "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+      },
+      {
+        selector: "TSEnumDeclaration",
+        message: "Avoid enums as they result in runtime code; TypeScript should be types only."
+      },
+      {
+        selector: "TSParameterProperty",
+        message: "Avoid parameter properties as they result in runtime code; TypeScript should be types only."
+      },
+      {
+        selector: "WithStatement",
+        message: "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+      }
+    ],
 
-    // Make everything work with .ts and .tsx as well
-    "import/extensions": [2, {
-      js: "never",
-      ts: "never",
-      tsx: "never",
-    }],
+    /**
+     * No overwriting built-in globals (for example URL)
+     * 
+     * See also: https://eslint.org/docs/rules/no-shadow
+     */
+    "no-shadow": [
+      "error",
+      { builtinGlobals: true }
+    ],
 
-    // Allow devDeps in test files
-    "import/no-extraneous-dependencies": [0, {
-      "devDependencies": ["**/*.test.*"],
-    }],
+
+    // ES6+ export/imports
+    /**
+     * No file extensions within the import source path
+     * 
+     * See also: https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+     */
+    "import/extensions": [
+      "error",
+      {
+        js: "never",
+        jsx: "never",
+        mjs: "never",
+        ts: "never",
+        tsx: "never",
+      }
+    ],
 
     // TODO: Discuss whether we should we prefer default exports (as in airbnb base) or no default exports (as follows)
     // "import/prefer-default-export": 0,
     // "import/no-default-export": 2,
 
-    // Sort import statements
-    "import/order": ["error", {
-      alphabetize: {
-        order: "asc",
-        caseInsensitive: true,
-      },
-      "newlines-between": "never"
-    }],
+    /**
+     * No duplicate imports
+     * 
+     * See also: https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
+     */
+    "import/no-duplicates": "error",
 
-    // Remove airbnb's ForOfStatement recommendation; we don't use regenerator-runtime anywyas,
-    // and we iterate over Sets in our libraries.
-    "no-restricted-syntax": [2, {
-      selector: "ForInStatement",
-      message: "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
-    }, {
-      selector: "LabeledStatement",
-      message: "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
-    }, {
-      selector: "WithStatement",
-      message: "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
-    }],
-
-    // Allow empty arrow functions, useful as defaults or for testing mocks
-    "@typescript-eslint/no-empty-function": [
-      "error", { "allow": ["arrowFunctions"] }
+    /**
+     * No dev dependencies except in tests
+     * 
+     * See also: https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+     */
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["**/*.test.*", "**/test/**/*.*"],
+      }
     ],
 
+    /**
+     * No unsorted import statements
+     * 
+     * See also: https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+     */
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "never"
+      }
+    ],
+
+
+    // TypeScript
+    /**
+     * Naming convention
+     * 
+     * See also: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+     */
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'forbid',
+        trailingUnderscore: 'forbid',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: ['typeParameter'],
+        format: ['PascalCase'],
+        custom: {
+          regex: "^[T,K,P]$",
+          match: true,
+        },
+      }
+    ],
+
+    /**
+     * No empty functions except arrow functions
+     * 
+     * Note: arrow functions are useful as defaults or for testing mocks
+     * 
+     * See also: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-empty-function.md
+     */
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      { allow: ["arrowFunctions"] },
+    ],
+
+    /**
+     * No unhandled promise rejection
+     * 
+     * See also: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-floating-promises.md
+     */
     "@typescript-eslint/no-floating-promises": "error",
 
-    // We allow underscores in some situations, such as internal_ or unstable_. Additionally,
-    // many of the libraries we use commonly use underscores, so disable this rule.
-    "@typescript-eslint/camelcase": [0],
+    /**
+     * No unused variables
+     * 
+     * See also: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+     */
+    "@typescript-eslint/no-unused-vars": "error",
   },
-}
+};
