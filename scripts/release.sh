3fe73bf8884274275777bcc63d1c29ca0473fad7
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-06-24 16:07:15 +0200

    ci: manually specify version to publish so lerna doesn't prompt

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,7 +13,7 @@ isPreRelease=$(node -pe 'require("./lerna.json").version.includes("-")')
 
 # Publish the release with lerna, which automatically detects pre-releases,
 # --no-verify-access is required as automation tokens can't list packages
-lerna publish --yes --no-verify-access --temp-tag --loglevel verbose
+lerna publish "$version" --yes --no-verify-access --temp-tag --loglevel verbose
 
 # Don't try creating the release if the publish failed:
 if [ $? -neq 0 ]; then
