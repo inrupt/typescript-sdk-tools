3bd8f34ef368d0c6853220424f9517c99f025011
Nicolas Seydoux <nseydoux@inrupt.com>
2022-11-29 13:29:19 +0100

    Add internal-playwright-testids package
    This package exports testids that are shared between tests fixtures and test apps.

---
+++ packages/internal-playwright-testids/index.ts
@@ -0,0 +1,5 @@
+export const TESTID_OPENID_PROVIDER_INPUT = "identityProviderInput";
+export const TESTID_LOGIN_BUTTON = "loginButton";
+export const TESTID_LOGOUT_BUTTON = "logoutButton";
+export const TESTID_ERROR_MESSAGE = "errorMessage";
+export const TESTID_SESSION_STATUS = "loggedInMessage";
