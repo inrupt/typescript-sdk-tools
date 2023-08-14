525a85a0d563e584bee162d81463f1b35f3e5cb7
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2023-08-14 18:14:51 +0200

    fixup! Use alternative selectors to support testid

--- packages/internal-playwright-helpers/src/pages/open-id.ts
+++ packages/internal-playwright-helpers/src/pages/open-id.ts
@@ -32,11 +32,14 @@ export class OpenIdPage {
   }
 
   async allow() {
+    // Class-based selector that will remain compatible with previous code
     const classBasedSelector = this.page.locator(".allow-button");
+    // Testid-based selector that will be compatible with newer releases
     const testidBasedSelector = this.page.getByTestId("prompt-allow");
+    // Once we no longer support ESS 2.1, we can remove the class-based selector and only use the testid-based one.
     await expect(classBasedSelector.or(testidBasedSelector)).toBeVisible();
     // Fallback selector to support class attributes, until testid supports is fully deployed.
-    const correctSelector = await classBasedSelector.isVisible() ? classBasedSelector : testidBasedSelector;
+    const correctSelector = await testidBasedSelector.isVisible() ? testidBasedSelector : classBasedSelector;
     await Promise.all([
       // It is important to call waitForNavigation before click to set up waiting.
       this.page.waitForURL("http://localhost*"),
