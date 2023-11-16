832543d14d50fff9e4326f1b55c89ef13e51a28f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-11-16 17:50:27 +0000

    fix: make the allow parameter optional (#483)
    * chore: make the allow parameter optional
    
    * chore: fix lint errors

--- packages/internal-playwright-helpers/src/flows/auth.ts
+++ packages/internal-playwright-helpers/src/flows/auth.ts
@@ -51,7 +51,7 @@ export class AuthFlow {
    * and completes login after being redirected to client.
    */
   async login(
-    options: { allow: boolean; timeout?: number } = { allow: true },
+    options: { allow?: boolean; timeout?: number } = {},
   ): Promise<void> {
     const timeoutOptions = { timeout: options.timeout ?? 15_000 };
     const testPage = new TestPage(this.page, this.openidProvider);
@@ -86,7 +86,10 @@ export class AuthFlow {
     }
 
     // TODO: handle allow === false
-    if (options.allow && OpenIdPage.isOnPage(new URL(this.page.url()))) {
+    if (
+      options.allow !== false &&
+      OpenIdPage.isOnPage(new URL(this.page.url()))
+    ) {
       completeLoginConditions.push(openIdPage.allow(timeoutOptions));
     }
     await Promise.all(completeLoginConditions);
