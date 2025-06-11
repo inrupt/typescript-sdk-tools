b1428ddd122a55980921607fc67ea0eb05c4509e
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2025-06-11 22:27:21 +0200

    Support comments in JSON

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -1,9 +1,27 @@
+// Copyright 2020 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import js from "@eslint/js";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 
 import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from 'eslint-plugin-import';
+import importPlugin from "eslint-plugin-import";
 import jest from "eslint-plugin-jest";
 import headers from "eslint-plugin-headers";
 import prettier from "eslint-plugin-prettier/recommended";
@@ -12,13 +30,12 @@ import hooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
   prettier,
   // JS config
- {
+  {
     ...js.configs.recommended,
-    files: ['**/*.js', '**/*.mjs'],
+    files: ["**/*.js", "**/*.mjs"],
   },
   // TS config
   ...tseslint.configs.recommended,
@@ -31,17 +48,26 @@ export default defineConfig([
     },
     rules: {
       "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/no-empty-function": ["error", {
-            allow: ["arrowFunctions"],
-        }],
-      "@typescript-eslint/consistent-type-imports": ["error", {
+      "@typescript-eslint/no-empty-function": [
+        "error",
+        {
+          allow: ["arrowFunctions"],
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
           prefer: "type-imports",
-      }],
+        },
+      ],
       "@typescript-eslint/return-await": ["error", "in-try-catch"],
     },
     // Lint imports based on TS module resolution.
-    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
-    files: ['**/*.ts', '**/*.tsx'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    files: ["**/*.ts", "**/*.tsx"],
     // This avoids requiring a dedicated tsconfig.eslint.json file in every repo.
     ignores: ["**/*.test.ts", "**/*.mock.ts"],
   },
@@ -49,7 +75,7 @@ export default defineConfig([
   {
     plugins: {
       react,
-      'react-hooks': hooks,
+      "react-hooks": hooks,
     },
     rules: {
       ...react.configs.recommended.rules,
@@ -59,78 +85,85 @@ export default defineConfig([
     },
     settings: {
       react: {
-        version: 'detect', // You can add this if you get a warning about the React version when you lint
+        version: "detect", // You can add this if you get a warning about the React version when you lint
       },
     },
-    files: ["**/*.jsx", "**/*.tsx"]
+    files: ["**/*.jsx", "**/*.tsx"],
   },
   // Globals config
-  { 
+  {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   // JSON linting
   {
     files: ["**/*.json"],
     plugins: { json },
-    language: "json/json"
+    // Support JSON with comments (e.g. tsconfig)
+    language: "json/jsonc",
   },
   // Markdown linting
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
-    extends: ["markdown/recommended"]
+    extends: ["markdown/recommended"],
   },
   // Tests linting
   {
     ...jest.configs["flat/recommended"],
     ...jest.configs["flat/style"],
     files: [
-        "**/*.test.ts",
-        "**/*.test.tsx",
-        "**/*.test.js",
-        "**/*.test.jsx",
-        "**/*.spec.ts",
-        "**/*.spec.tsx",
-        "**/*.spec.js",
-        "**/*.spec.jsx",
+      "**/*.test.ts",
+      "**/*.test.tsx",
+      "**/*.test.js",
+      "**/*.test.jsx",
+      "**/*.spec.ts",
+      "**/*.spec.tsx",
+      "**/*.spec.js",
+      "**/*.spec.jsx",
     ],
     plugins: { jest },
     rules: {
-        "no-shadow": ["warn", {
-            allow: [
-                "describe",
-                "it",
-                "jest",
-                "expect",
-                "beforeEach",
-                "beforeAll",
-                "afterEach",
-                "afterAll",
-            ],
-        }],
-        "@typescript-eslint/no-explicit-any": ["off"],
+      "no-shadow": [
+        "warn",
+        {
+          allow: [
+            "describe",
+            "it",
+            "jest",
+            "expect",
+            "beforeEach",
+            "beforeAll",
+            "afterEach",
+            "afterAll",
+          ],
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": ["off"],
     },
   },
   // License headers config
   {
     plugins: {
-      headers
+      headers,
     },
     rules: {
-        "headers/header-format": ["error", {
+      "headers/header-format": [
+        "error",
+        {
           source: "file",
           path: "LICENSE",
-          style: "line"
-        }]
-      }
+          style: "line",
+        },
+      ],
+    },
   },
   // The .next directory is generated by NextJS and should not be linted.
-  globalIgnores(["**/.next/", "**/dist/", "**/docs/"])
+  globalIgnores(["**/.next/", "**/dist/", "**/docs/"]),
 ]);
